0e5ac515c3330e8a4170bc56fcc73818264d16ed
ravigajul <ravi.gajul@yahoo.com>
2019-06-10 17:42:12 +0530

    secon commit

---
+++ src/main/java/RestAssured/RestAssured/GetData.java
@@ -0,0 +1,19 @@
+package RestAssured.RestAssured;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+/**
+ * Hello world!
+ *
+ */
+public class  GetData
+{
+   public static void main(String[] args) {
+    	
+    	Response resp= RestAssured.get("https://samples.openweathermap.org/agro/1.0/weather?polyid=5aaa8052cbbbb5000b73ff66&appid=b1b15e88fa797225412429c1c50c122a1");
+        System.out.println(resp.getStatusCode());
+        System.out.println(resp.asString());
+        
+    }
+}
