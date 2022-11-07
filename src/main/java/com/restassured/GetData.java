168eb51cd1d0418425120669e1686ba84e0a4b18
ravi.gajul <ravi.gajul@yahoo.com>
2022-11-07 19:16:46 +0530

    removed .classpath and settings files and deleted target folder. Also added new json file and new method for RestAssured.

--- src/main/java/com/restassured/GetData.java
+++ src/main/java/com/restassured/GetData.java
@@ -1,19 +1,41 @@
 package com.restassured;
 
+import java.io.File;
+
+import org.json.simple.JSONObject;
+
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 /**
  * Hello world!
  *
  */
-public class  GetData
-{
-   public static void main(String[] args) {
-    	
-    	Response resp= RestAssured.get("https://samples.openweathermap.org/agro/1.0/weather?polyid=5aaa8052cbbbb5000b73ff66&appid=b1b15e88fa797225412429c1c50c122a1");
-        System.out.println(resp.getStatusCode());
-        System.out.println(resp.asString());
-        
-    }
+public class GetData {
+	public static void main(String[] args) {
+		/*
+		 * JSONObject jsonObj = new JSONObject(); jsonObj.put("name", "morpheus");
+		 * jsonObj.put("job", "leader");
+		 */
+		String jsondata="{\r\n"
+				+ "    \"name\": \"morpheus\",\r\n"
+				+ "    \"job\": \"leader\"\r\n"
+				+ "}";
+		String filePath= System.getProperty("user.dir");
+		System.out.println(filePath);
+		RestAssured.baseURI = "https://reqres.in";
+		RequestSpecification httpRequest = RestAssured.given();
+		httpRequest.header("Content-Type", "application/json");
+		httpRequest.header("Accept", "applicationjson");
+		httpRequest.body(new File(filePath + File.separator+ "src"+ File.separator+"main"+File.separator+"java"+File.separator+"com"+File.separator+"restassured"+File.separator+"data.json"));
+		//httpRequest.body(jsondata);
+
+		// Response after post method Response response =
+		Response response = httpRequest.post("/api/users");
+		System.out.println(response.getStatusCode() + "-->" + response.getBody().asString());
+
+	}
+
 }
