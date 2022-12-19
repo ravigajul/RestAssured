745acddd4e50383c32cce2965859aa02d088e435
ravi.gajul <ravi.gajul@yahoo.com>
2022-12-19 22:42:21 +0530

    added logic to compare and update a file line

--- src/main/java/com/restutils/FileHelper.java
+++ src/main/java/com/restutils/FileHelper.java
@@ -9,44 +9,53 @@ import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class FileHelper {
 
 	public static void main(String[] args) {
+		String fileName = "C:\\Users\\anjal\\Downloads\\diff.txt";
+		String lineToUpdate = "K3*MDQA";
+		String updatedLine = "K3*Updated line!";
+		checkAndUpdateFile(fileName, lineToUpdate, updatedLine);
 		// TODO Auto-generated method stub
-		Path path1;
-		Path path2;
-		FileWriter fw;
-		BufferedWriter bw;
-		PrintWriter prnt;
-		String filesPath = "C:\\Users\\anjal\\Downloads";
-		final File srcFolder = new File(filesPath + File.separator + "src");
-		final File destFolder = new File(filesPath + File.separator + "dest");
+		/*
+		 * Path path1; Path path2; FileWriter fw; BufferedWriter bw; PrintWriter prnt;
+		 * String filesPath = "C:\\Users\\anjal\\Downloads"; final File srcFolder = new
+		 * File(filesPath + File.separator + "src"); final File destFolder = new
+		 * File(filesPath + File.separator + "dest"); try { fw = new
+		 * FileWriter(filesPath + File.separator + "diff.txt"); bw = new
+		 * BufferedWriter(fw); prnt = new PrintWriter(bw); for (final File srcFile :
+		 * srcFolder.listFiles()) { if (!srcFile.isDirectory()) {
+		 * System.out.println(srcFile.getName()); for (final File destFile :
+		 * destFolder.listFiles()) { if (!destFile.isDirectory()) { path1 =
+		 * Paths.get(srcFile.getAbsolutePath()); path2 =
+		 * Paths.get(destFile.getAbsolutePath()); if
+		 * (path1.getFileName().toString().equalsIgnoreCase(path2.getFileName().toString
+		 * ())) { filesCompareByLine(path1, path2, prnt); break; } } } } } prnt.flush();
+		 * } catch (IOException e) { // TODO Auto-generated catch block
+		 * e.printStackTrace(); }
+		 */
+	}
+
+	public static void checkAndUpdateFile(String fileName, String lineToUpdate, String updatedLine) {
 		try {
-			fw = new FileWriter(filesPath + File.separator + "diff.txt");
-			bw = new BufferedWriter(fw);
-			prnt = new PrintWriter(bw);
-			for (final File srcFile : srcFolder.listFiles()) {
-				if (!srcFile.isDirectory()) {
-					System.out.println(srcFile.getName());
-					for (final File destFile : destFolder.listFiles()) {
-						if (!destFile.isDirectory()) {
-							path1 = Paths.get(srcFile.getAbsolutePath());
-							path2 = Paths.get(destFile.getAbsolutePath());
-							if (path1.getFileName().toString().equalsIgnoreCase(path2.getFileName().toString())) {
-								filesCompareByLine(path1, path2, prnt);
-								break;
-							}
-						}
-					}
+			// Read the file into a List of Strings
+			Path path = Paths.get(fileName);
+			List<String> lines = Files.readAllLines(path);
+
+			// Update the line in the List
+			for (int i = 0; i < lines.size(); i++) {
+				if (lines.get(i).contains(lineToUpdate)) {
+					lines.set(i, updatedLine);
+					break;
 				}
 			}
-			prnt.flush();
+			// Write the updated content back to the file
+			Files.write(path, lines);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
 	public static void filesCompareByLine(Path path1, Path path2, PrintWriter prnt) {
